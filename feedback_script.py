6897f8705a30fba0daacb70ffdfebe05e3835b11
Benjamin Gonzales <benz@utexas.edu>
2022-05-22 14:05:09 -0500

    allowed multiple personalized information to be stored and processed in a single line

--- feedback_script.py
+++ feedback_script.py
@@ -4,6 +4,19 @@ import sys
 import subprocess
 import re
 
+#global variables
+# TODO: fill in correct usr
+usr = "bgg646"
+stored_answers = dict()
+f = open('/home/bgg646/feedback/stored_answers.txt', 'r')
+for line in f.readlines():
+    key, regex_value = re.split("~~~", line)
+    stored_answers[key] = (False, regex_value)
+    
+
+
+
+
 #helper functions
 def match(regex, str):
     match_obj = re.search(regex, str)
@@ -32,8 +45,49 @@ def get_line(str):
     index = str.find("\n")
     return str if index == -1 else str[:index + 1]
 
+def extract(answer, cur_line, k, key):
+
+    # store the regex pattern that matches the expected answer
+    (a ,iso_re) = stored_answers[key]
+
+    # find span of key in answer
+    match_obj = re.search(k, answer)
+    beg, end = match_obj.span()
+
+    # isolate the the student's info
+    # missing case when end in .* (not needed right now thought)
+
+    # personal info that we are trying to extract is surrounded by .*
+    if beg <= len(answer) and answer[beg - 2:beg] == ".*" and end + 2 <= len(answer) and answer[end: end + 2]:
+        left = re.sub(iso_re + answer[end:], "", cur_line)
+        right = re.sub(answer[:beg] + iso_re, "", cur_line)
+        result = re.sub(left, "", re.sub(right, "", cur_line))
+    else:
+        # accounting for the case when the personal info is at the beg or end of the string
+        result = cur_line
+        if answer[:beg]: result = re.sub(answer[:beg], "", result)
+        if answer[end:]: result = re.sub(answer[end:], "", result)
+
+    return result
+
+def use_stored_answer(answer, cur_line):
+    keys = re.findall("<r>.*?</r>", answer)
+    for k in keys:
+        # get rid of the tags
+        key = re.sub("<r>", "", k)
+        key = re.sub("</r>", "", key)
+        # unpack: boolean and stored_answer
+        (stored, stored_answer) = stored_answers[key]
+        if(not stored):
+            stored_answer = extract(answer, cur_line, k, key)
+            stored_answers[key] = True, stored_answer
+        answer = re.sub(k, stored_answer, answer)
+    return answer
+
 def cleared(expected, output):
     cur_line = get_line(output)
+    # TODO: ensure they are outputting a valid output
+    expected = use_stored_answer(expected, cur_line)
     if(expected == "\n"):
         p_expected = "a blank line"
     if(cur_line == "\n"):
@@ -42,7 +96,7 @@ def cleared(expected, output):
         p_expected = "\"" + expected.strip() + "\""
         p_cur_line = "\"" + cur_line.strip()     + "\""
 
-    if(match(expected, cur_line)):
+    if(match(expected, cur_line)):                
         output = output[len(cur_line):]
         print("cleared. You successfully outputted", p_expected)
         return output
@@ -56,18 +110,11 @@ def cleared(expected, output):
 
 
 
+
 def test():
-    # TODO: fill in correct usr
-    usr = "bgg646"
-    stored_answers = dict()
-
-
-
-
-
     # check if user is in the right directory
     their_pwd, = bytes_to_string(subprocess.check_output(['pwd']))
-    correct_pwd = "/home/" + usr + "/lab0-part2.*"
+    correct_pwd = "/home/" + usr + "/lab0-part2"
     nested_directory = "/home/" + usr + "/.+/lab0-part2.*"
 
 
@@ -90,8 +137,7 @@ def test():
 
 
     # Check for syntax errors
-    script_path = correct_pwd[0:len(correct_pwd) - 2] + "/Your_script/your_script.py"
-    
+    script_path = correct_pwd + "/Your_script/your_script.py"
     p = subprocess.Popen(['python', script_path], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     o, e = p.communicate() 
     output, error = bytes_to_string(o, e)
@@ -120,9 +166,11 @@ def test():
     
 
     # Check their output
+    # TODO unhardcode feedback paths
     f = open("/home/bgg646/feedback/answers.txt", 'r')
     for line in f.readlines():
         if not output:
+            print(stored_answers)
             return
         expected = line
         output = cleared(expected, output)
--- test.py
+++ test.py
@@ -1,4 +1,6 @@
 import subprocess
+import re
+
 def tup(*args):
     print(args)
     list = [] 
@@ -14,3 +16,31 @@ print(p.returncode)
 print("\nasd;lkfjdskf j"[1:])
 str = "\n"
 print(str[0:1] == "\n")
+
+val = None
+if(val):
+    print("hello")
+
+match_o1 = re.findall("[0-2]", "g6h3kdsfj3kdfj6")
+print(type(match_o1))
+print(match_o1)
+print(match_o1 == None)
+
+print("19\n")
+
+
+
+
+
+# begins or ends with .*
+    # 
+re1 = "a 99 b"
+print(re1)
+re1 = re.sub("a.*", "", re1)
+print(re1)
+re1 = re.sub(".*b", "", re1)
+print(re1)
+
+ans_ex_1 = "longhorn_rule.*<r>name</r>.*<r>age</r>.*3\.14.*(True|False).*<class 'str'>"
+mo_ex_1 = re.search(ans_ex_1, "longhorn_rule: Benjamin Gonzales 19 3.14 True <class 'str'>")
+print(mo_ex_1)
